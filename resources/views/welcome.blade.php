ee0d0bf5c0fb102359270ac8b074e6f62cea93c2
Ben Argo <ben@benargo.com>
2020-03-09 22:16:50 +0530

    Update 2020

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,21 +13,30 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            <div class="content">
-                <div class="title m-b-md">
+            <div class="mx-3 text-center">
+                <h1 class="display-1 mb-6">
                     Hobnobs
-                </div>
-
-                <div class="links">
-                    <a href="https://benargo.com" title="benargo.com">Ben Argo</a>
-                    <a href="http://www.harrietcody.com" title="harrietcody.com">Harriet Cody</a>
-                    <a href="http://dushyantkanungo.com" title="dushyantkanungo.com">Dushyant Kanungo</a>
-                    <a href="http://gunnarpetzall.com" title="gunnarpetzall.com">Gunnar Petzäll</a>
+                </h1>
+                <div class="row">
+                    <div class="col mb-3">
+                        <a href="https://benargo.com" title="benargo.com">Ben<br />Argo</a>
+                    </div>
+                    <div class="w-100 d-md-none"></div>
+                    <div class="col mb-3">
+                        <a href="http://www.harrietcody.com" title="harrietcody.com">Harriet<br />Cody</a>
+                    </div>
+                    <div class="w-100 d-md-none"></div>
+                    <div class="col mb-3">
+                        <a href="http://dushyantkanungo.com" title="dushyantkanungo.com">Dushyant<br />Kanungo</a>
+                    </div>
+                    <div class="w-100 d-md-none"></div>
+                    <div class="col mb-3">
+                        <a href="http://gunnarpetzall.com" title="gunnarpetzall.com">Gunnar<br />Petzäll</a>
+                    </div>
                 </div>
             </div>
         </div>
         <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}"></script>
         <script type="text/javascript">
             (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
             ga('create', 'UA-23790873-10', 'auto');
